7e05e6ab2d5eac4e8e9f3778d43d69f47078bae4
Gabriel Silva <gabriel.jesus@dcomp.ufs.br>
2020-03-06 21:05:53 -0300

    implementacao de somente uma valvula solenoide

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 import machine
 import time
+
+#Função para leitura do ADC através do pino D3 do NodeMCU
 from machine import Pin
 
+#ler o potenciomentro
 ################# variaveis globais ########################
 atuador_sole1 = Pin(15, Pin.OUT) 
 atuador_sole2 = Pin(16, Pin.OUT)
-umidade = float(input("Digite a humidade:"))
+umidade = 1000 # input do usuario
 pino_sensor1 = 0
 pino_sensor2 = 2
 ############################################################
 
 #ler o potenciomentro 
-def ler_ADC():
+"""def ler_ADC():
+    adc = machine.ADC(0)
+    var= adc.read()
     adc_1 = machine.ADC(pino_sensor1)
     var = adc_1.read()
     print('ADC VALUE.. = %.2f' % var)
+"""
 
 
 # retorna o valor do sensor 
@@ -25,16 +31,20 @@ def rtrn_valor_sensor1():
     """
     adc = machine.ADC(pino_sensor1)
     var_temp = adc.read()
+    print('ADC VALUE.. = %.2f' % (10000/var_temp))
     return var_temp
 
 
-def retrn_valor_sensor2():
-    pass
+atuador_sole1.value(0)
 
-
-atuador_sole1.value(1)
-while(True):
-    if rtrn_valor_sensor1 == umidade:
-        atuador_sole1.value(0)
+while True:
+    value = rtrn_valor_sensor1()
+    
+    if value >= umidade:
+        atuador_sole1.value(1)
+    else:
+      atuador_sole1.value(0)
+      
     ler_ADC()
-    time.sleep_ms(1000)
+    time.sleep_ms(50) 
+  
