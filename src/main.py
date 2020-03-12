f8e1795a9cfa784ee16f13661a2740b1b4327f8e
Gabriel Silva <gabriel.jesus@dcomp.ufs.br>
2020-03-12 13:49:30 -0300

    implementação da valvula central

--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@ from machine import Pin
 ################# variaveis globais ########################
 atuador_sole1 = Pin(19, Pin.OUT) 
 atuador_sole2 = Pin(18, Pin.OUT)
+atuador_central = Pin(21, Pin.OUT)
 umidade = 4050 # input do usuario
 pino_sensor1 = Pin(34, Pin.IN) 
 pino_sensor2 = Pin(35, Pin.IN)
 ############################################################
 
+
+def percentual(leitura):
+    return 100* leitura / 4095
 
 # retorna o valor do sensor 
 def retorna_valores_sensores():
@@ -18,13 +22,14 @@ def retorna_valores_sensores():
     adc1 = adc1.read()
     adc2 = machine.ADC(pino_sensor2)
     adc2 = adc2.read()
-    return (adc1, adc2)
+    return (percentual(adc1), percentual (adc2))
 
 
 atuador_sole1.value(0)
 
 while True:
     valor_sensor = retorna_valores_sensores()
+    umidade = percentual(umidade)
     
     if valor_sensor[0] >= umidade:
         atuador_sole1.value(1)
@@ -34,5 +39,9 @@ while True:
         atuador_sole2.value(1)
     else:
         atuador_sole2.value(0)
-    time.sleep_ms(50) 
+    if atuador_sole1.value() == 1 and atuador_sole2.value() == 1:
+        atuador_central.value(1)
+    else:
+        atuador_central.value(0)
+    time.sleep_ms(50)
   
