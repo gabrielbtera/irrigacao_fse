118621686801ea06c7c074f772f26dca8a4b59ab
Valter Guilherme Silva de Souza <valterguilhermess@gmail.com>
2020-03-12 22:09:44 -0300

    Renderizar pagina web no navegador do cliente

--- src/main.py
+++ src/main.py
@@ -11,9 +11,57 @@ pino_sensor1 = Pin(34, Pin.IN)
 pino_sensor2 = Pin(35, Pin.IN)
 ############################################################
 
+def renderizador():
+    html = """<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Irrigação</title>
+    <style>
+      .quadrante {
+        height: 350px;
+        width: 350px;
+        line-height: 350px;
+        margin: auto;
+        margin-bottom: 10px;
+      }
+      .button {
+        background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        transition: background-color 1s;
+      }
+      button:hover{
+        background-color: darkgreen ;
+      }
+    </style>
+  </head>
+  <body>
+    <div style="text-align: center;">
+      <h1>IRRIGAÇÃO INTELIGENTE, RESPONSIVA E FORMIDÁVEL</h1>
+      <h2>VÁVULA CENTRAL:<strong id="state">ON/OFF</strong></h2>
+      <button class="button">TRAVA MANUAL</button>
+      <h2><strong>QUADRANTES</strong></h2>
+      <h1 class="quadrante" style="background-color: rgb(67, 133, 2);"><strong>Q1</strong></h1>
+      <h1 class="quadrante" style="background-color: rgb(67, 133, 2);"><strong>Q2</strong></h1>
+    </div>
+    <script>
+      
+    </script>
+  </body>
+</html> """
+
+    return html
+
 
 def percentual(leitura):
     return 100* leitura / 4095
+
 
 # retorna o valor do sensor 
 def retorna_valores_sensores():
@@ -24,22 +72,43 @@ def retorna_valores_sensores():
     return (percentual(adc1), percentual (adc2))
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
 umidade = percentual(umidade)
+
 while True:
     valor_sensor = retorna_valores_sensores()
-    
-    
+    conn, addr = s.accept()
+    conn.send('HTTP/1.1 200 OK\n')
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)
+
     if valor_sensor[0] >= umidade:
         atuador_sole1.value(0)
+    
     else:
       atuador_sole1.value(1)
+    
     if valor_sensor[1] >= umidade:
         atuador_sole2.value(0)
+    
     else:
         atuador_sole2.value(1)
+    
     if atuador_sole1.value() == 0 and atuador_sole2.value() == 0:
         atuador_central.value(0)
+    
     else:
         atuador_central.value(1)
+    
     time.sleep_ms(50)
-  
+    
+    response = renderizador()
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
