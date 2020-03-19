b4aba1b8154f1c1f40e26c4c183542e184682af5
Gabriel Silva <gabriel.jesus@dcomp.ufs.br>
2020-03-19 20:39:46 -0300

    config ponto de acesso main e alteração de cores dos quadrantes web

--- src/boot.py
+++ src/boot.py
@@ -1,7 +1,7 @@
 try:
-  from usocket import socket
+  from usocket import socket, AF_INET, SOCK_STREAM
 except:
-  from socket import socket
+  from socket import socket, AF_INET, SOCK_STREAM
 
 import esp
 esp.osdebug(None)
@@ -10,10 +10,24 @@ import gc
 gc.collect()
 
 import network
+"""
 ap = network.WLAN(network.AP_IF)
 
 if not ap.active():
   ap.active(True)
 
 ap.config(essid='brain', password='thisisaprototype')
-print(ap.ifconfig())
+print(ap.ifconfig())"""
+
+ssid = ''
+password = ''
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+while not station.isconnected():
+  pass
+
+print("Connection successful!")
+print(station.ifconfig())
